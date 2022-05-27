10007eded456187ae13352d24a8e421d535d281e
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-05-26 20:42:28 -0500

    Create a new form and validated it with all specifications

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  contactForm: FormGroup = new FormGroup({});
+
+  constructor( private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    const numReg = new RegExp(/^\+[0-9]*$/)
+    this.contactForm = this.fb.group({
+      user: ['', [Validators.pattern(/^([A-Za-z]*)*([0-9]*)*$/)]],
+      useremail: ['', [Validators.email]],
+      userphone: ['', [Validators.pattern(numReg)]],
+      Message: ['', Validators.maxLength(10)]
+    })
+  }
+
+
+
+}
